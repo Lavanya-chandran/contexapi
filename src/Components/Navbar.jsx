81cf5de8b96dd4a30fa2d785820a8a0c8bc63fa7
Lavanya-chandran <rclavanya04@gmail.com>
2024-08-16 18:19:11 +0530

    complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,6 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -26,4 +25,4 @@ function Navbar() {
   )
 }
 
-export default Navbar
+export default Navbar;
